c98efa4b5e4ef417febbc0343f7595f5fe1396ea
annette-arrigucci <annette.arrigucci@gmail.com>
2016-08-10 12:09:09 -0400

    added View to Assign Users to Projects

--- BugTracker/Controllers/ProjectUserViewController.cs
+++ BugTracker/Controllers/ProjectUserViewController.cs
@@ -10,12 +10,11 @@ namespace BugTracker.Controllers
 {
     public class ProjectUserViewController : Controller
     {
-
         private ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: ProjectUserView
         [Authorize(Roles ="Admin, Project Manager")]
-        public ActionResult Edit(int projectId)
+        public ActionResult Edit(int? projectId)
         {
             var projectModel = new ProjectUserViewModel();
             var project = db.Projects.Find(projectId);
@@ -42,13 +41,40 @@ namespace BugTracker.Controllers
             }
         }
 
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Edit([Bind(Include = "ProjectId,SelectedUsers,NotSelectedUsers")] ProjectUserViewModel projModel)
-        //{
-
-        //}
-
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProjectId,SelectedUsers,NotSelectedUsers")] ProjectUserViewModel projModel)
+        {
+            var pId = projModel.ProjectId;
+            var helper = new ProjectUserHelper();
+            //if no users are selected, remove all users from project
+            if (projModel.SelectedUsers == null)
+            {
+                var usersToRemove = helper.UsersInProject(pId);
+                foreach(var user in usersToRemove)
+                {
+                    helper.RemoveUserFromProject(user, pId);
+                }
+            }
+            else
+            {
+                for(var i=0; i<projModel.SelectedUsers.Length; i++)
+                {
+                    if(!helper.IsUserInProject(projModel.SelectedUsers[i], pId))
+                    {
+                        helper.AddUserToProject(projModel.SelectedUsers[i], pId);
+                    }   
+                }
+                for(var i=0; i<projModel.NotSelectedUsers.Length; i++)
+                {
+                    if(helper.IsUserInProject(projModel.SelectedUsers[i], pId))
+                    {
+                        helper.RemoveUserFromProject(projModel.SelectedUsers[i], pId);
+                    }
+                }
+            }
+            return RedirectToAction("Index", "Projects");
+        }
         public List<UserInfoViewModel> getUserInfo (List<string> userIds)
         {
             //set up a list to contain user info objects
